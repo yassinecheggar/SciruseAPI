875c7631f70490d49f6a8f705a530d95e32b1a7a
yassi <yassi@DESKTOP-T1OPA60>
2020-02-09 00:04:47 +0100

    controller added for film

--- SciruseDb/src/main/java/com/example/demo/SciruseDbApplication.java
+++ SciruseDb/src/main/java/com/example/demo/SciruseDbApplication.java
@@ -1,14 +1,8 @@
 package com.example.demo;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 @SpringBootApplication
